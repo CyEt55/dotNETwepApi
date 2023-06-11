d5b8ca5e2b59f298a64ca41c1464f83814f89f48
CyEt55 <albertz_2011@hotmail.com>
2023-06-11 11:24:13 -0500

    cors modifications and further testing

--- Program.cs
+++ Program.cs
@@ -3,14 +3,18 @@ using dotNETwepApi.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+const string allowedAddress = "http://localhost:4200";
+// http://localhost:4200 is used because Angular serves the Front-End on port 4200 by default
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<PracticeContext>(opt => opt.UseNpgsql("Host=localhost;Database=angularpractice;Username=me;Password=password"));
-builder.Services.AddCors(opt => opt.AddDefaultPolicy(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddCors(opt => opt.AddDefaultPolicy(builder => builder.WithOrigins(allowedAddress).AllowAnyMethod()));
 
 var app = builder.Build();
 
+app.UseHttpLogging();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
